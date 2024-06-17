3dddeccb0e7eb7307722d90cbc7c942882d9be5d
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2024-06-17 14:54:02 +0200

    Create header component and setup redux store  (#1)
    * Create blank Header component
    
    * Update path in tsconfig.json
    
    * Update nextjs config
    
    * Add redux and redux/toolkit
    
    * Add clsx
    
    * Update tsconfig.json paths
    
    * Create store wrapper for NextJS
    
    * Update layout and import bg3 logo
    
    * Add navigation and styles for header component

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,4 @@
 export { default as api } from './api'
+export { default as store } from './store'
+export { type AppStore, type AppDispatch, type RootState } from './store'
+export { useAppDispatch, useAppSelector, useAppStore } from './config'
