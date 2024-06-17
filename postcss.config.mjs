a385ab96f33637c2596dec3ded8bdf1b0f5cac98
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2024-06-17 11:01:04 +0200

    Add Mantine UI

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,7 +3,17 @@ const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-preset-mantine': {},
+    'postcss-simple-vars': {
+      variables: {
+        'mantine-breakpoint-xs': '36em',
+        'mantine-breakpoint-sm': '48em',
+        'mantine-breakpoint-md': '62em',
+        'mantine-breakpoint-lg': '75em',
+        'mantine-breakpoint-xl': '88em',
+      },
+    },
   },
-};
+}
 
-export default config;
+export default config
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from 'react'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ColorSchemeScript, MantineProvider } from '@mantine/core'
 
+import '@mantine/core/styles.css'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,7 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang={'en'}>
-      <body className={inter.className}>{children}</body>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body className={inter.className}>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   )
 }
