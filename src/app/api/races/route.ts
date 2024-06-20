a1253bd34392d39ac029cf54e4499f33f1491828
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-06-20 14:05:24 +0200

    Remove unecessary page and return correct api data

--- src/app/api/races/route.ts
+++ src/app/api/races/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from 'next/server'
+import dataLoader from 'db/dataLoader'
+import { ApiDbScopeEnum } from 'features/common/models'
 
-export function GET() {
-  return NextResponse.json([{ id: 100, name: 'Test' }], { status: 200 })
+export async function GET() {
+  const data = await dataLoader(ApiDbScopeEnum.races)
+
+  return NextResponse.json(data, { status: 200 })
 }
