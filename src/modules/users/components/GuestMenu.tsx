37dacb889be8eb2773daf4d05755e9751750f4c6
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-08-10 12:58:30 +0200

    Style guest auth menu

--- src/modules/users/components/GuestMenu.tsx
+++ src/modules/users/components/GuestMenu.tsx
@@ -8,13 +8,24 @@ import classes from './users.module.css'
 
 export default function GuestMenu() {
   return (
-    <nav className={'flex items-center gap-4'}>
-      <Link href={'/login'}>{'Login'}</Link>
+    <nav className={'flex items-center gap-6'}>
+      <Link
+        href={'/login'}
+        className={clsx(
+          'uppercase font-medium text-white transition-colors hover:text-amber-200 focus:text-white focus-visible:text-amber-200',
+          classes.login,
+        )}
+      >
+        {'Login'}
+      </Link>
       <Button
-        radius={'xs'}
+        radius={0}
         component={Link}
         href={'/register'}
-        className={clsx('uppercase font-medium', classes.register)}
+        className={clsx(
+          'uppercase font-medium h-auto px-6 py-4 bg-amber-200 text-black transition-colors hover:bg-white hover:text-black focus:bg-amber-200 focus:text-black focus-visible:bg-white',
+          classes.register,
+        )}
       >
         {'Register'}
       </Button>
