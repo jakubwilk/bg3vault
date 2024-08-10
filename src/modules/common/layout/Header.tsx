a33b2c53365b6537ab21b1126c342ed0614ff62f
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-08-10 21:59:58 +0200

    Update header styles

--- src/modules/common/layout/Header.tsx
+++ src/modules/common/layout/Header.tsx
@@ -1,14 +1,26 @@
 'use client'
 
+import { useMemo } from 'react'
 import { clsx } from 'clsx'
+import { useAppSelector } from 'services/config'
 
 import Menu from './Menu'
 
 import classes from './layout.module.css'
 
 export default function Header() {
+  const { hasTopRatedBuilds } = useAppSelector((state) => state.builds)
+
+  const headerClassName = useMemo(() => {
+    if (hasTopRatedBuilds) {
+      return 'md:max-h-[85vh] lg:max-h-[70vh]'
+    }
+
+    return 'md:max-h-[30vh] lg:max-h-[25vh]'
+  }, [hasTopRatedBuilds])
+
   return (
-    <header className={clsx('h-full md:h-screen md:max-h-[85vh] lg:max-h-[70vh]', classes.header)}>
+    <header className={clsx('h-full md:h-screen', headerClassName, classes.header)}>
       <div className={'container mx-auto'}>
         <div className={'w-full px-4 flex justify-between items-center gap-4'}>
           <div>{'logo'}</div>
--- src/services/store.ts
+++ src/services/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { authSlice } from 'auth/store'
+import { buildsSlice } from 'builds/store'
 
 export const store = () => {
   return configureStore({
     reducer: {
       auth: authSlice.reducer,
+      builds: buildsSlice.reducer,
     },
   })
 }
