cb90bdeb7312a66feedacfb29463ec9aa2823c17
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-08-11 13:25:36 +0200

    Create news home feed component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { CreateBuildHomeButton } from 'builds/components'
+import { NewsHomeWrapper } from 'news/components'
 
 export default function Home() {
   return (
     <main className={'min-h-screen'}>
       <div className={'container mx-auto min-h-[inherit]'}>
-        <div className={'min-h-[inherit] flex flex-col px-4'}>
+        <div className={'min-h-[inherit] flex flex-col gap-6 px-4'}>
           <CreateBuildHomeButton />
+          <NewsHomeWrapper />
         </div>
       </div>
     </main>
