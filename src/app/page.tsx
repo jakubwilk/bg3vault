c7ef5c226a9a6143a8dae041b4d67aa6ab2dd437
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2024-08-27 21:27:46 +0200

    Create auth pages in next routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <main className={'min-h-full'}>
       <div className={'container mx-auto min-h-[inherit]'}>
-        <div className={'min-h-[inherit] flex flex-col gap-6 px-4'}>{'test\r'}</div>
+        <div className={'min-h-[inherit] flex flex-col gap-6 px-4'}>
+          <div className={'flex items-center justify-center gap-4'}>
+            <Link href={'/login'}>{'Logowanie'}</Link>
+            <Link href={'/register'}>{'Rejestracja'}</Link>
+          </div>
+        </div>
       </div>
     </main>
   )
