585c6abc4c97324ac29124786187162e7cae7dd2
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2024-09-19 16:47:06 +0200

    Create axios api instance

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,60 @@
-const api = {}
+import { notifications } from '@mantine/notifications'
+import axios, { AxiosInstance } from 'axios'
+
+const api: AxiosInstance = axios.create({
+  baseURL: process.env['BASE_API_URL'] || '',
+  timeout: 360000,
+})
+
+api.interceptors.request.use(
+  (config) => {
+    // const token = localStorage.getItem('access_token')
+    // config.headers.Authorization = `Bearer ${token}`
+
+    // console.log('instance.interceptors.request.config', config)
+    return config
+  },
+  () => {
+    // console.log('instance.interceptors.request.error', error)
+  },
+)
+
+api.interceptors.response.use(
+  (response) => {
+    // console.log('instance.interceptors.response.response', response)
+    return response
+  },
+  (error) => {
+    console.log('instance.interceptors.response.error', error)
+    if (error?.response) {
+      if (error.response.status === 404) {
+        notifications.show({
+          color: 'red',
+          title: '404 Code',
+          classNames: { root: 'rounded-none' },
+          message:
+            'An attempt was made to send a query to a resource that does not exist. Contact the service administrator',
+        })
+        return Promise.reject(error.response)
+      }
+
+      if ([400, 402, 403].includes(error.response.status)) {
+        notifications.show({
+          color: 'red',
+          message: error.response?.data?.details,
+        })
+        return Promise.reject(error.response)
+      }
+
+      if (error.response.status === 500) {
+        notifications.show({
+          color: 'red',
+          message: error.response?.data?.title,
+        })
+        return Promise.reject(error.response)
+      }
+    }
+  },
+)
 
 export default api
