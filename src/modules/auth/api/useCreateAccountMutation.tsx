bf25855632b1b5f88d29e62ac10522c4c0e9f5ce
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2024-09-19 16:46:58 +0200

    Use axios instead of nextjs fetch

--- src/modules/auth/api/useCreateAccountMutation.tsx
+++ src/modules/auth/api/useCreateAccountMutation.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useMutation } from '@tanstack/react-query'
+import api from 'services/api'
 
 export interface ICreateAccountRequest {
   email: string
@@ -10,18 +11,9 @@ export interface ICreateAccountRequest {
 export const CreateAccountKey = 'CREATE_ACCOUNT_KEY'
 
 const createAccount = async ({ email, username, password }: ICreateAccountRequest) => {
-  try {
-    const response = await fetch('/api/auth/create', {
-      method: 'POST',
-      body: JSON.stringify({ email, username, password }),
-    })
-    const data = await response.json()
+  const { data } = await api.post('/api/auth/create', { email, username, password })
 
-    return data
-  } catch (err) {
-    console.log('err', err)
-    return err
-  }
+  return data
 }
 
 export default function useCreateAccountMutation() {
