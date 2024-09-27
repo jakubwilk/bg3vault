1509008858e3d04fc00247396d6319a99cd6e9a7
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2024-09-27 10:29:17 +0200

    Update mapper

--- src/services/mapper.ts
+++ src/services/mapper.ts
@@ -2,12 +2,16 @@ import { User } from '@prisma/client'
 import { IAuthUser } from 'auth/models'
 
 export function getUserDTO(user: User): IAuthUser {
-  const { id, username, email, role } = user
+  console.log('user', user)
+  const { id, username, email, role, isActivated, isBanned, isDeleted } = user
 
   return {
     id,
     username,
     email,
     role,
+    isActivated,
+    isBanned,
+    isDeleted,
   }
 }
