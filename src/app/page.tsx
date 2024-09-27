bd50a2f142c4906fc78990184b522c1a192f3853
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2024-09-28 00:50:24 +0200

    Update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+import { cookies } from 'next/headers'
 import Link from 'next/link'
+import api from 'services/fetch'
+import { AccountActivationBar } from 'users/components'
 
-export default function Home() {
+async function getUser() {
+  const token = cookies().get('auth')?.value
+  const uid = cookies().get('uid')?.value
+
+  return await api.get(`api/users/get?auth=${token}&uid=${uid}`)
+}
+
+export default async function Home() {
+  const { data } = await getUser()
+
   return (
     <main className={'min-h-full'}>
+      <AccountActivationBar data={data} />
       <div className={'container mx-auto min-h-[inherit]'}>
         <div className={'min-h-[inherit] flex flex-col gap-6 px-4'}>
           <div className={'flex items-center justify-center gap-4'}>
