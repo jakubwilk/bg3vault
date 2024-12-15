5a8f91ff3f18ee216cb5020c1b67f55e930cec92
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2024-12-16 00:16:56 +0100

    Commit after changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,5 @@
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
-import createMiddleware from 'next-intl/middleware'
-import { routing } from 'i18n'
-
-const intlMiddlewre = createMiddleware(routing)
-
-export function middleware(request: NextRequest) {
-  const intlResponse = intlMiddlewre(request)
-
-  if (intlResponse) {
-    return intlResponse
-  }
-
-  return NextResponse.next()
-}
+export function middleware() {}
 
 export const config = {
-  matcher: [
-    '/',
-    '/(pl|en)/:path*',
-    '/((?!api|_next/static|assets|_next/image|favicon.ico|globals.css).*)',
-  ],
+  matcher: ['/', '/((?!api|_next/static|assets|_next/image|favicon.ico|globals.css).*)'],
 }
