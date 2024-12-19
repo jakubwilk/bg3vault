5c2cf9a9bbda7e733bbc1ca6df5c35a09ae0ffe4
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2024-12-19 12:13:36 +0100

    Create PasswordInput component

---
+++ src/modules/common/components/passwordInput/PasswordInput.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { useMemo } from 'react'
+import {
+  PasswordInput as MantinePasswordInput,
+  PasswordInputProps as MantinePasswordInputProps,
+} from '@mantine/core'
+import { clsx } from 'clsx'
+
+import classes from './styles.module.css'
+
+type TMantinePasswordInputPropsOmit = Omit<MantinePasswordInputProps, 'size' | 'classNames'>
+
+type TPasswordInputClassNames = {
+  wrapper?: string
+  input?: string
+  section?: string
+  root?: string
+  label?: string
+  required?: string
+  description?: string
+  error?: string
+}
+
+interface ITextInputProps extends TMantinePasswordInputPropsOmit {
+  classNames?: TPasswordInputClassNames
+}
+
+export default function PasswordInput({ classNames = {}, ...rest }: ITextInputProps) {
+  const passwordInputClasses = useMemo(() => {
+    const { wrapper, input, section, root, label, required, description, error } = classNames
+    const defaultClasses = {
+      wrapper: clsx(wrapper ?? ''),
+      section: clsx(section ?? ''),
+      root: clsx(classes.root, root ?? ''),
+      required: clsx(required ?? ''),
+      error: clsx(error ?? ''),
+    }
+
+    return {
+      input: clsx(classes.input, classes.inputMedium, input ?? ''),
+      label: clsx(classes.label, label ?? ''),
+      description: clsx(description ?? ''),
+      ...defaultClasses,
+    }
+  }, [classNames])
+
+  return <MantinePasswordInput radius={0} classNames={passwordInputClasses} {...rest} />
+}
