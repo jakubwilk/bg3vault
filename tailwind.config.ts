4413fe7b9a152057023a3bf5b7117239bbcc8dbe
Jakub Wilk <jakub.wilk@jakubwilk.pl>
2025-01-26 15:18:10 +0100

    Update tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,12 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    screens: {
+      sm: '48em',
+      md: '62em',
+      lg: '75em',
+      xl: '88em',
+    },
     extend: {
       colors: {
         background: 'var(--background)',
